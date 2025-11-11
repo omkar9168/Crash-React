bf6b55ca0d671bdaf290d529f26076549f650997
omkar2024 <omkarghadge2126@gmail.com>
2025-11-11 11:41:21 +0530

    first commit

---
+++ 2 create-react-comp/create-first-comp/src/App.jsx
@@ -0,0 +1,11 @@
+import KgButton from "./KgButton"
+import Para from "./para"
+function App() {
+  return ( <div>
+    <h1>This is best react course</h1>
+    <KgButton></KgButton>
+  </div>
+  )
+}
+
+export default App
---
+++ 2 create-react-comp/create-first-comp/src/KgButton.jsx
@@ -0,0 +1,8 @@
+function KgButton(){
+  return(
+    <button>
+    Like this video
+    </button>
+  )
+}
+export default KgButton;
