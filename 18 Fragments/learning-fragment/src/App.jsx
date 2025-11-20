83938fd61b0443e85d0eea2bbc6cd94e7188445e
omkar2024 <omkarghadge2126@gmail.com>
2025-11-20 16:37:11 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   18 Fragments/learning-fragment/src/App.jsx
    	modified:   18 Fragments/learning-fragment/src/components/Container.jsx
    	modified:   18 Fragments/learning-fragment/src/components/Container.module.css
    	modified:   18 Fragments/learning-fragment/src/components/ErrorMessage.jsx
    	new file:   18 Fragments/learning-fragment/src/components/FoodInput.jsx
    	new file:   18 Fragments/learning-fragment/src/components/FoodInput.module.css
    	modified:   18 Fragments/learning-fragment/src/components/FoodItems.jsx
    	modified:   18 Fragments/learning-fragment/src/components/Item.jsx
    	modified:   18 Fragments/learning-fragment/src/components/Item.module.css
    	new file:   Projects/5-calculator-version-2/calculator/.gitignore
    	new file:   Projects/5-calculator-version-2/calculator/README.md
    	new file:   Projects/5-calculator-version-2/calculator/eslint.config.js
    	new file:   Projects/5-calculator-version-2/calculator/index.html
    	new file:   Projects/5-calculator-version-2/calculator/package-lock.json
    	new file:   Projects/5-calculator-version-2/calculator/package.json
    	new file:   Projects/5-calculator-version-2/calculator/public/vite.svg
    	new file:   Projects/5-calculator-version-2/calculator/src/App.jsx
    	new file:   Projects/5-calculator-version-2/calculator/src/App.module.css
    	new file:   Projects/5-calculator-version-2/calculator/src/assets/react.svg
    	new file:   Projects/5-calculator-version-2/calculator/src/components/ButtonsContainer.jsx
    	new file:   Projects/5-calculator-version-2/calculator/src/components/ButtonsContainer.module.css
    	new file:   Projects/5-calculator-version-2/calculator/src/components/Display.jsx
    	new file:   Projects/5-calculator-version-2/calculator/src/components/Display.module.css
    	new file:   Projects/5-calculator-version-2/calculator/src/main.jsx
    	new file:   Projects/5-calculator-version-2/calculator/vite.config.js
    	new file:   Projects/6-todo-app-version-3/.gitignore
    	new file:   Projects/6-todo-app-version-3/README.md
    	new file:   Projects/6-todo-app-version-3/eslint.config.js
    	new file:   Projects/6-todo-app-version-3/index.html
    	new file:   Projects/6-todo-app-version-3/package-lock.json
    	new file:   Projects/6-todo-app-version-3/package.json
    	new file:   Projects/6-todo-app-version-3/public/vite.svg
    	new file:   Projects/6-todo-app-version-3/src/App.css
    	new file:   Projects/6-todo-app-version-3/src/App.jsx
    	new file:   Projects/6-todo-app-version-3/src/assets/react.svg
    	new file:   Projects/6-todo-app-version-3/src/components/AddTodo.jsx
    	new file:   Projects/6-todo-app-version-3/src/components/AppName.jsx
    	new file:   Projects/6-todo-app-version-3/src/components/AppName.module.css
    	new file:   Projects/6-todo-app-version-3/src/components/TodoItem.jsx
    	new file:   Projects/6-todo-app-version-3/src/components/TodoItems.jsx
    	new file:   Projects/6-todo-app-version-3/src/components/TodoItems.module.css
    	new file:   Projects/6-todo-app-version-3/src/components/WelcomeMessage.jsx
    	new file:   Projects/6-todo-app-version-3/src/components/WelcomeMessage.module.css
    	new file:   Projects/6-todo-app-version-3/src/main.jsx
    	new file:   Projects/6-todo-app-version-3/vite.config.js
    	modified:   notes.tldr

--- 18 Fragments/learning-fragment/src/App.jsx
+++ 18 Fragments/learning-fragment/src/App.jsx
@@ -1,25 +1,30 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import Container from "./components/Container";
+import FoodInput from "./components/FoodInput";
+import { useState } from "react";
+
 function App() {
-  let foodItems = ["Sabzi", "Salad", "Roti", "Milk", "Ghee", "Green Vegetable"];
+  let [foodItems, setFoodItems] = useState([]);
+
+  const onKeyDown = (event) => {
+    if (event.key === "Enter") {
+      let newFoodItem = event.target.value;
+      event.target.value = "";
+      let newItems = [...foodItems, newFoodItem];
+      setFoodItems(newItems);
+    }
+  };
 
   return (
     <>
       <Container>
-        <h1 className="food-heading"> Healthy Food </h1>
+        <h1 className="food-heading">Healthy Food</h1>
+        <FoodInput handleKeyDown={onKeyDown}></FoodInput>
         <ErrorMessage items={foodItems}></ErrorMessage>
         <FoodItems items={foodItems}></FoodItems>
-      </Container>
-
-      <Container>
-        <p>
-          {" "}
-          Above is the list of healthy food that are good for your health and
-          well being
-        </p>
       </Container>
     </>
   );
--- 18 Fragments/learning-fragment/src/components/FoodItems.jsx
+++ 18 Fragments/learning-fragment/src/components/FoodItems.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import Item from "./Item";
 
-const FoodItems = ({items}) => {
+const FoodItems = ({ items }) => {
+  let [activeItems, setActiveItems] = useState([]);
+
+  let onBuyButton = (item) => {
+    let newItems = [...activeItems, item];
+    setActiveItems(newItems);
+  };
+
   return (
-  <ul className="list-group">
+    <ul className="list-group">
       {items.map((item) => (
-        <Item key={item} foodItem={item}></Item>
-       ))}
+        <Item
+          key={item}
+          foodItem={item}
+          bought={activeItems.includes(item)}
+          handleBuyButton={(event) => onBuyButton(item, event)}
+        ></Item>
+      ))}
     </ul>
-  )
+  );
 };
 
 export default FoodItems;
